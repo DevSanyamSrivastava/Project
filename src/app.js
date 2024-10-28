d0eb8f01e82a6a73e17bc603996c48311848b5f5
DevSanyamSrivastava <sanyam.fullstackdev@gmail.com>
2024-10-28 16:37:34 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -15,4 +15,14 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//routes import
+import userRouter from './routes/user.routes.js'
+
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+
+//  http://127.0.0.1:8000/api/v1/users/register
+
+
 export {app}
